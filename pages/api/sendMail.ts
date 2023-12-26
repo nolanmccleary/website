f0bb08aa06a23019fbf3594ff4f0710d0700dd51
Nolan <62817066+ncm88@users.noreply.github.com>
2023-12-26 10:27:18 -0800

    add initial form error checking

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,20 +1,23 @@
-// pages/api/sendMail.ts
-
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('ON');
-  if (req.method === 'POST') {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ success: false, error: 'Method Not Allowed' });
+  }
+
+  try {
     const data = req.body;
 
-    // Here you would typically handle sending the email
-    // using services like SendGrid, Nodemailer, Mailgun etc.
+    // Here, add validation for the data
+    // For example, check if the email is valid, the message is not empty, etc.
+    // If validation fails, return an error response.
 
-    // Mock sending for this example
-    console.log('Data received', data);
+    // If validation passes, send the email (using a service like SendGrid, Nodemailer, etc.)
+    // For now, we'll simulate successful email sending
 
-    res.status(200).json({ success: true });
-  } else {
-    res.status(405).end(); // Method Not Allowed
+    res.status(200).json({ success: true, message: 'Email sent successfully.' });
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,97 +3,109 @@ import React, { useState } from 'react';
 import Animate from '@/components/Animate';
 import Layout from '@/components/Layout';
 
-interface Props {
-  placeholder?: any;
-}
-
-const actionClasses =
-  'relative inline-flex justify-center w-full sm:w-10 h-10 px-3 py-2 bg-gray-900 hover:bg-gray-800 text-gray-400 hover:text-gray-300 border border-gray-500 rounded-lg text-sm font-medium default-transition default-focus';
-
-const Contact = (props: Props): JSX.Element => {
-  // Remove useState hook for formData since we'll use FormData API
+const Contact = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [submissionMessage, setSubmissionMessage] = useState('');
+  const [submissionSuccess, setSubmissionSuccess] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    console.log('howdy');
     e.preventDefault();
-    // Create a new FormData object, passing in the form as the argument
     const formData = new FormData(e.currentTarget);
+
     try {
       const response = await fetch('/api/sendMail', {
         method: 'POST',
-
-        // The headers may be omitted, as the fetch API sets the correct header for FormData
-        body: formData, // Send the FormData object directly
+        body: formData,
       });
 
       const data = await response.json();
+      setIsSubmitted(true);
+
       if (data.success) {
-        console.log('Email sent successfully!');
+        setSubmissionSuccess(true);
+        setSubmissionMessage(data.message || 'Your message has been sent successfully!');
       } else {
-        console.log('Failed to send email');
+        setSubmissionSuccess(false);
+        setSubmissionMessage('Failed to reach server. Please try again.');
       }
     } catch (error) {
-      console.error('There was an error sending the email', error);
+      setIsSubmitted(true);
+      setSubmissionSuccess(false);
+      console.error('There was an error sending the email:', error);
+      setSubmissionMessage('Internal server error. Please try again.');
     }
   };
 
   return (
     <Layout>
-      <div className="my-24 mx-2 sm:mx-6 lg:mb-28 lg:mx-8">
-        <div className="relative max-w-xl mx-auto">
-          <Animate animation={{ y: [50, 0], opacity: [0, 1] }} transition={{ delay: 0.1 }}>
-            <div className="bg-gray-900 bg-opacity-75 border border-gray-500 rounded-lg transition ease-in-out duration-300 p-6">
-              <div className="flex flex-col items-center justify-center w-full">
-                <h1 className="text-gray-300 text-lg font-bold mb-4">Contact Me</h1>
-                <form className="w-full" onSubmit={handleSubmit}>
-                  <div className="flex flex-col space-y-4">
-                    <div className="flex flex-col space-y-2">
-                      <label htmlFor="name" className="text-sm text-gray-400">
-                        Your Name
-                      </label>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="John Doe"
-                        className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
-                        required
-                      />
+      <div className="my-24 mx-2 sm:mx-6 lg:mb-28 lg:mx-8 text-center">
+        {isSubmitted ? (
+          <div className="bg-gray-900 bg-opacity-75 border border-gray-500 rounded-lg transition ease-in-out duration-300 p-6">
+            <span
+              role="img"
+              aria-label={submissionSuccess ? 'check' : 'cross'}
+              className={`text-4xl ${submissionSuccess ? 'text-green-500' : 'text-red-500'}`}
+            >
+              {submissionSuccess ? '✅' : '❌'}
+            </span>
+            <p className="text-gray-300 text-lg font-bold mt-4">{submissionMessage}</p>
+          </div>
+        ) : (
+          <div className="relative max-w-xl mx-auto">
+            <Animate animation={{ y: [50, 0], opacity: [0, 1] }} transition={{ delay: 0.1 }}>
+              <div className="bg-gray-900 bg-opacity-75 border border-gray-500 rounded-lg transition ease-in-out duration-300 p-6">
+                <div className="flex flex-col items-center justify-center w-full">
+                  <h1 className="text-gray-300 text-lg font-bold mb-4">Contact Me</h1>
+                  <form className="w-full" onSubmit={handleSubmit}>
+                    <div className="flex flex-col space-y-4">
+                      <div className="flex flex-col space-y-2">
+                        <label htmlFor="name" className="text-sm text-gray-400">
+                          Your Name
+                        </label>
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          placeholder="John Doe"
+                          className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
+                          required
+                        />
+                      </div>
+                      <div className="flex flex-col space-y-2">
+                        <label htmlFor="email" className="text-sm text-gray-400">
+                          Your Email
+                        </label>
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          placeholder="john@example.com"
+                          className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
+                          required
+                        />
+                      </div>
+                      <div className="flex flex-col space-y-2">
+                        <label htmlFor="message" className="text-sm text-gray-400">
+                          Your Message
+                        </label>
+                        <textarea
+                          name="message"
+                          id="message"
+                          rows={5}
+                          className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
+                          required
+                        ></textarea>
+                      </div>
+                      <button type="submit" className="bg-gray-700 hover:bg-gray-600 text-gray-300 p-3 rounded-md">
+                        Send
+                      </button>
                     </div>
-                    <div className="flex flex-col space-y-2">
-                      <label htmlFor="email" className="text-sm text-gray-400">
-                        Your Email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="john@example.com"
-                        className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
-                        required
-                      />
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                      <label htmlFor="message" className="text-sm text-gray-400">
-                        Your Message
-                      </label>
-                      <textarea
-                        name="message"
-                        id="message"
-                        rows={5}
-                        className="p-3 border rounded-md bg-gray-800 text-gray-300 border-gray-500"
-                        required
-                      ></textarea>
-                    </div>
-                    <button type="submit" className="bg-gray-700 hover:bg-gray-600 text-gray-300 p-3 rounded-md">
-                      Send
-                    </button>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
-            </div>
-          </Animate>
-        </div>
+            </Animate>
+          </div>
+        )}
       </div>
     </Layout>
   );
