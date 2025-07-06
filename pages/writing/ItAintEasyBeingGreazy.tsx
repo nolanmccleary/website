82c1f3b0ecdfa88165f99281e5cda1d66dbd8407
Nolan <62817066+ncm88@users.noreply.github.com>
2025-07-06 19:33:24 -0400

    it aint easy being greazy

---
+++ pages/writing/ItAintEasyBeingGreazy.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from 'next';
+
+import Animate from '@/components/Animate';
+import Layout from '@/components/Layout';
+
+
+const Framework: NextPage = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen flex text-left justify-center py-12">
+        <div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
+          <Animate
+            as="p"
+            animation={{ opacity: [0, 1], scale: [0.75, 1] }}
+            className="max-w-xs mt-4 md:mt-8 mx-auto text-base text-left text-gray-400 dark:text-gray-300 sm:text-lg md:text-xl md:max-w-3xl"
+            transition={{ delay: 0.5 }}
+          >
+
+        It Ain't Easy Being Greazy (How to Toronto, Summer Edition)
+        <br></br>
+        06/17/2025
+        <br></br><br></br>
+        Sup Fam, Yung Crodigy back at it again. If the Leafs can make it to game seven, then this city deserves a round two. In the name of King Drizzy the First, Messiah of the 6ix and my personal Ubermensch, I present to you the second installment of the fan favourite How to Toronto series. 
+
+        <br></br><br></br>
+        Step 1: Conceal Your Presence
+        <br></br>
+        As my favourite non-Torontonian coke dealer once said, ain't no love in the heart of the city. What this means is that danger lurks around every corner, and you dun never know when da opp mans finna pop out wit dat dimaz durag and da toolie for the purposes of engaging in premeditated, non-consensual yutenization (yute-wasting ting). You need to be proactive with your defenses, and the best way to do that is to make sure that you're never a target in the first place. 
+        <br></br><br></br>
+        This is why it is imperative that your wardrobe includes significant amounts of camouflage attire. The obvious first-choice item here is the camouflage hat. Browning is the traditional choice, but it is recommended that you substitute for a more upscale variant as soon as possible so that you aren't misclassified as a prole when entering an establishment. In the wintertime, the camouflage hat can also be combined with the peacoat and supporting impractical jackets to create an avant-garde aesthetic like no other, especially when paired with a good set of loafers or pastel Jordans, but that's beside the point. For added stealth on the golf course, a good camouflage polo is also recommended.
+        <br></br><br></br>
+        At this point, you are basically a human F22 Raptor. The opps will never be able to see you, and even if they did, they would quickly realize that an engagement with you would be completely suicidal, because you are clearly packing heat and know how to use it. Little do they know that the closest you've come to operating a firearm was shooting your cousin Brandon Kensington in the ass with a Nerf rifle (no headshots allowed except on Christmas and Victoria Day), the thought of killing an animal makes you sick, and if someone told you that they had a 33 by their bedside, you would instinctively assume that they brought Auston Matthews over for a slumber party and rounded down.
+        <br></br><br></br>
+
+        Step 2: Get Bricked Up
+        <br></br>
+        It's a summertime in the 6ix, and something remarkable is underway. Every weekend throughout the summer, Torontonians by the thousands leave the warmth and familiarity of the urban sprawl to embark on a grueling two-to-three-hour journey as they return to their ancestral spawning grounds in Muskoka. The journey is perilous, and many don't make it, but for the ones that do, they are met with a truly breathtaking spectacle: peace and fucking quiet.
+        <br></br><br></br>
+        During this time, the Torontonians relinquish the usual comforts of their metrosexual lifestyle in favour of seeking refuge within their humble 1.7-million-dollar brick cabins. These aren't ordinary cabins, however; for in order to attract potential mates during the spawning season, Torontonians refer to them as cottages, thereby indicating an abundance of cheese. If you can't find the keys to your cottage, please review your inheritance, the details should be visible right above the generational milk holder section. 
+        <br></br><br></br>
+        There are many activities to engage in during spawning season in Muskoka, including but not limited to: frolicking in lakes, jumping off docks, not having beaches, and engaging in recreational alcoholism to cope with the absence of said beaches. However, make sure you wear your finest camouflage hat, Huntley, at all times during these endeavours. You never know when opps finna conduct a slip'n slide (amphibious assault).
+        <br></br><br></br>
+
+        Step 3: Endure
+        <br></br>
+        If it's not on Strava, it didn't happen. However, we can also invert this truth: If something happened, it must have been on Strava. Thus, all things that have happened are on Strava. This proves the existence of God, because a non-agentic creator wouldn't have set up a Strava account. From this, it can be inferred that Strava exists at a higher level in the natural order than God himself, but we all already knew that. This naturally begs the question, if Strava created God, who created Strava? This is known as the Unmoved Mover Paradox and has been the subject of continuous debate for millennia. With the nature of reality confirmed, we are now free to ponder how we should best make use of our time in it. Thankfully, the answer is quite obvious: to maximize our lives, we simply need to upload as much of them as possible onto Strava.
+        <br></br><br></br>
+        Strava isn't strictly a Toronto ting, but Torontonians take their Strava very seriously. Every time you run, ride, walk, frolick, or shit, it must go on Strava. If it didn't, how would everyone else know how much more disciplined, healthy, and productive you are than them? That was a trick question. They wouldn't know because you never were. This is blatantly unacceptable, and both your ancestral bloodline and your progeny will resent you for it. Put that shit on Strava.
+        <br></br><br></br>
+
+        Conclusion:
+        <br></br>
+        With its virtuous inhabitants, strong sense of community, and unwavering commitment towards signalling individual excellence, it's no wonder that Toronto is commonly referred to as the Vanderhoof of the East. This city provides a lot of unique challenges that should not be taken lightly, but it also rewards those who brave these challenges head-on through the continued and unconditional love and support of the Toronto People: a unified and proud community that only wants the best for their city, their flat province, their country, and their species. You might have noticed that I never explicitly defined where Toronto is; is Mississauga Toronto? What about Markham? What do you even call Guelph? Is it part of Toronto too, or is it just Ontario's Chilliwack? The truth is that Toronto is more than a location; it's an ideal, a thought, an abstract way of being that each of us can instinctively recognize at a deeply personal level. We know where Toronto is because we know where it isn't: in the darkness, the silence, the wearing of highly practical jackets, and in the possession of dogs that were bred to serve a purpose. We must each carry this truth with us in our hearts as we toosie slide in the footsteps of the Messiah every minute of every hour of every day. This is the only way to truly reach Toronto, and the only way to truly reach salvation.
+
+
+
+          </Animate>          
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Framework;
