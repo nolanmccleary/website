55d1fc2260ccead4dfc09bad83b2d1aed26d13c9
Nolan <62817066+ncm88@users.noreply.github.com>
2025-09-21 13:06:55 -0700

    ubc capstone

---
+++ pages/writing/UBCCapstone.tsx
@@ -0,0 +1,139 @@
+import type { NextPage } from 'next';
+
+import Animate from '@/components/Animate';
+import Layout from '@/components/Layout';
+
+
+const Framework: NextPage = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen flex text-left justify-center py-12">
+        <div className="max-w-md sm:max-w-lg md:sm:max-w-2xl lg:sm:max-w-3xl w-full space-y-8 text-center">
+          <Animate
+            as="p"
+            animation={{ opacity: [0, 1], scale: [0.75, 1] }}
+            className="max-w-xs mt-4 md:mt-8 mx-auto text-base text-left text-gray-400 dark:text-gray-300 sm:text-lg md:text-xl md:max-w-3xl"
+            transition={{ delay: 0.5 }}
+          >
+
+UBC Capstone
+<br></br>
+09/21/2025
+<br></br><br></br>
+PROJECT #69:
+<br></br>
+Penetrator MK. 9 - Life-Sized Autonomous Bull Walrus
+<br></br><br></br>
+CATEGORY
+<br></br>
+Research, sustainability
+<br></br><br></br>
+PRIMARY AREA
+<br></br>
+Deep Sustainability (DeepSus)
+<br></br><br></br>
+SECONDARY AREA
+<br></br>
+AI
+<br></br><br></br>
+
+TERTIARY AREA
+<br></br>
+Machine Learning
+<br></br><br></br>
+
+QUATERNARY AREA
+<br></br>
+Robotics
+<br></br><br></br>
+
+ORGANIZATION
+<br></br>
+Latvian Frank (we don’t know who he is either but here’s his phone number: +371 2345 6789)
+<br></br><br></br>
+
+BACKGROUND
+<br></br>
+Every year in Northern Canada, hundreds of people fall victim to ravenous packs of disgruntled polar bears out on the hunt. The bears are mad that we melted their sea ice, so as revenge they have been launching surprise grocery runs where the offending village folk are the groceries. These creatures are large, hungry, ravenous, and disgruntled. They are also clever, playfully tapping on the doors of unsuspecting houses before revealing to the victims that their Uber Eats order semantics have been inverted. Unfortunately, the bear’s lighthearted antics are also commonly mis-interpreted, and frequently end with the bear being shot, killed, and turned into a set of decidedly unstylish garments to be worn in poor taste by the unjustly gruntled barb- townspeople. The rate of these attacks has skyrocketed as the polar bears become more and more justly disgruntled from seeing their close friends and family being turned into poorly constructed fur hats and knockoff Uggs while the townspeople become more and more unjustly gruntled as a result of getting to turn a 1200-lb arctic killing machine into a Siberian Durag and a pair of Eskimo Shit Kickers. A peaceful solution is needed in order to ensure that no further life is lost, and no more distasteful garments are created – thankfully, that peaceful solution is already present in nature.
+<br></br><br></br>
+
+
+OBJECTIVES
+<br></br>
+Your task will be to design a full-size, fully autonomous replica of the polar bear’s natural deterrent: The Rutting Bull Walrus. The key design objectives are as listed below:
+<br></br><br></br>
+
+1.	The polar bear sees that there is a very real, very large male walrus present.
+<br></br><br></br>
+2.	The polar bear sees that the very real, very large male walrus is sufficiently aroused to engage the bear in combat.
+<br></br><br></br>
+3.	Through a combination of strategic bellows and a tight slide deck, the very real, very large male walrus successfully convinces the bear of its future ability to hypothetically engage the bear in real-world combat when it eventually ascends from the rank of WIT (Walrus-In-Training) to P.Bull (Professional Bull Walrus, it is a federal offence for unlicensed male walruses to advertise themselves as bull walruses without the P.Bull designation). 
+<br></br><br></br>
+4.	The aforementioned tight slide deck that the very real, very large male walrus will use to convince the polar bear that it is indeed a threat. This slide deck will also be used to convince the evaluation committee of your ability to act like an engineer. 
+<br></br><br></br>
+
+Phase 1 – Bellowing (49%):
+<br></br>
+The system must be able to lie on its stomach and bellow loudly. This will be a significant milestone as it captures approximately 95% of what a bull walrus does on a baily dasis and will thus net the team a mark of up to 49%. 
+<br></br><br></br>
+Phase 2 – Combat (0.5%):
+<br></br>
+Time permitting, the project should also be able to demonstrate its ability to neutralize a healthy and optimally fasted adult male polar bear in close quarters combat in a way that gets funnier and funnier the longer you think about it. This will be done through a tight slide deck as a WIT is not legally allowed to penetrate a healthy and optimally fasted adult male polar bear unless it is under the direct supervision of a P.Bull. Inside the slide deck there are no rules (save for those listed inside the 267-page RULES document available on Canvas as well as all rules previously mentioned in this document), only that the polar bear is starving (but in a strong, ravenous sort of way, not in a weak emaciated sort of way) and the walrus is in heat (but in a strong, roid rage sort of way, not in a suspicious Diddy sort of way). As this approximately captures 0.5% of what a bull walrus does on a baily dasis (with the other 4.5% going to clam consumption and fornication), the team will receive the remaining 0.5% of their grade from this part. 
+<br></br><br></br>
+
+Phase 3 – Victory (Not for marks):
+<br></br>
+When the optimally fasted adult male polar bear sees the Penetrator Mk. 9 Life-Sized Autonomous Bull Walrus (technically a Life-Sized Autonomous WIT), two things will go through his mind in this exact order: Firstly, that the bull walrus (technically a WIT) effectively defends his professional credibility by way of strategic bellows and a tight slide deck (WIT’s are not eligible for consumption). Secondly, that the construction of the walrus was equitably distributed amongst all five equally capable team members who have each done their due diligence to actually learn all necessary skills that the program thus far has pretended to teach. This will make the polar bear proud, and it will leave the village out of respect for the craftsmanship it is beholden to as well as the unnerving contents of the tight slide deck bellowed at it detailing its grizzly defeat should it attempt to consume the walrus after it has attained its P.Bull status.
+<br></br><br></br>
+
+Phase 4 – Slide Deck (50.5%): 
+<br></br>
+As mentioned earlier, a maximum of 49.5% of your grade may be obtained from actually building the thing, the other 50.5% is determined by how well you show it off. As the managerial class of the future, it is imperative that you learn how to present artifacts of absolutely zero real-world value to people who don’t know any better for the sake of personal advancement. This is the most important part of the project and a passing grade in this sub-section is required.
+<br></br><br></br>
+
+DELIVERABLES
+<br></br>
+1.	Robotic bull walrus (technically WIT) in heat (make sure to outline the thermal shielding safety procedures used): 49.5%. 
+<br></br>
+2.	Tight slide deck: 50.5%. 
+<br></br><br></br>
+
+WHAT DEGREE OF ACCESS DO YOU EXPECT STUDENTS TO HAVE TO YOUR ORGANIZATION’S FACILITIES
+<br></br>
+No
+<br></br><br></br>
+
+CONTRIBUTIONS ORGANIZATION IS ABLE TO PROVIDE
+<br></br>
+See above
+<br></br><br></br>
+
+ADDITIONAL CONSIDERATIONS
+<br></br>
+To solve a problem or explore a technology related to our organization's products or services. To recruit or network with students graduating from an undergraduate engineering program. To contribute to the engineering profession through education and mentorship., To engage or network with the electrical and computer engineering department at UBC.
+This project is a repeat or continuation of a past capstone project: Penetrator MK-8 - Life-Sized Autonomous Bull Walrus
+<br></br><br></br>
+
+PROJECT IS PROPRIETARY?
+<br></br>
+Required - I will use the UBC IPA and NDA forms. 
+<br></br>
+
+(Frank owns your ass)
+<br></br><br></br>
+
+KEY WORDS
+<br></br>
+Penetrator, DeepSus, Latvia
+
+          </Animate>          
+        </div>
+      </div>
+        * JK my capstone is gas
+        <br></br>
+
+    </Layout>
+  );
+};
+
+export default Framework;
